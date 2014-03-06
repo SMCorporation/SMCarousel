b6c6b55a11b8f5959268020e14a8c65eb3dc4a28
Sergey Pirogov <s.pirogov@111min.com>
2014-03-06 19:37:51 +0200

    setup pods

---
+++ SMCarouselProj/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  SMCarouselProj
+//
+//  Created by Sergey Pirogov on 06.03.14.
+//  Copyright (c) 2014 SM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SMCarouselProj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SMCarouselProj
+//
+//  Created by Sergey Pirogov on 06.03.14.
+//  Copyright (c) 2014 SM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
